24a5817d5552845b52ce00805ab7049fa4418040
cwilliams2020-new <75428203+cwilliams2020-new@users.noreply.github.com>
2023-08-09 14:16:15 +0100

    Scripts needed to remove variables.

---
+++ Emulator_remove_vars1.R
@@ -0,0 +1,24 @@
+# CW - Script to clear unnecessary variables, except those listed below, rather than placing each of these lines into Loading/Emul_in*
+
+to.remove <- ls()
+
+# New variables
+to.remove <- c(to.remove[!grepl("runemulator", to.remove)])
+to.remove <- c(to.remove[!grepl("runBC", to.remove)])
+to.remove <- c(to.remove[!grepl("runsaving", to.remove)])
+to.remove <- c(to.remove[!grepl("runstatsplots", to.remove)])
+to.remove <- c(to.remove[!grepl("simnumber", to.remove)])
+to.remove <- c(to.remove[!grepl("allvars", to.remove)])
+to.remove <- c(to.remove[!grepl("sens3", to.remove)])
+to.remove <- c(to.remove[!grepl("savefigs", to.remove)])
+
+# Existing (Nat's) variables
+to.remove <- c(to.remove[!grepl("EmulVars", to.remove)])
+to.remove <- c(to.remove[!grepl("Sim", to.remove)])
+to.remove <- c(to.remove[!grepl("noice", to.remove)])
+to.remove <- c(to.remove[!grepl("filepath", to.remove)])
+to.remove <- c(to.remove[!grepl("^model_input_mod", to.remove)], "to.remove")
+to.remove <- c(to.remove[!grepl("^model_output_mod", to.remove)], "to.remove")
+to.remove <- c(to.remove[!grepl("^Exp_num", to.remove)], "to.remove")
+
+rm(list=to.remove)
